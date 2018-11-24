0f1573c19dd3b7a9530cd57e7b838a4a03dd6684
mattcronan.webdev <mattcronan.webdev@gmail.com>
2018-11-24 00:07:19 -0600

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //Boilerplate EF Framework
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
